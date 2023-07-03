3fd7c69a75d7a2a7e5849dcdd3a01eab6c35af89
GaoNeng-wWw <gaonenggg@outlook.com>
2023-07-03 20:54:07 +0800

    feat: seo优化
    fix: 删除构建产物

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -55,7 +55,8 @@ export default defineNuxtConfig({
         ws: {
             hostname: 'localhost'
         }
-    }
+    },
+    base: 'content',
   },
   css: ['vue-devui/style.css', '@devui-design/icons/icomoon/devui-icon.css', '~/assets/styles/common.scss'],
   postcss: {
@@ -93,27 +94,13 @@ export default defineNuxtConfig({
           drop_debugger: true,
         }
       },
-      // rollupOptions: {
-      //   output: {
-      //     assetFileNames: assetInfo => {
-      //       var info = assetInfo.name!.split('.')
-      //       var extType = info[info.length - 1]
-      //       if (
-      //         /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/i.test(assetInfo.name!)
-      //       ) {
-      //         extType = 'media'
-      //       } else if (/\.(png|jpe?g|gif|svg)(\?.*)?$/.test(assetInfo.name!)) {
-      //         extType = 'img'
-      //       } else if (/\.(woff2?|eot|ttf|otf)(\?.*)?$/i.test(assetInfo.name!)) {
-      //         extType = 'fonts'
-      //       }
-      //       return `_nuxt/static/${extType}/[name]-[hash][extname]`
-      //     },
-      //     chunkFileNames: '_nuxt/static/js/[name]-[hash].js',
-      //     entryFileNames: '_nuxt/static/js/[name]-[hash].js'
-      //   }
-      // }
+      rollupOptions:{
+        output:{
+          chunkFileNames: 'nuxt/chunk/chunk-[hash].js',
+          assetFileNames: 'nuxt/assets/assert-[hash][extname]'
+        }
+      }
     },
   },
-  ssr: false,
+  ssr: true,
 })
